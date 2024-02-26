7335239e236812b85a5ea82045477cbbabfb6100
emiliavillanen <140176466+emiliavillanen@users.noreply.github.com>
2024-02-26 13:56:45 +0200

    Minigame (#1)
    * vaihdoin scenen nimen minigameksi
    
    * added minigame scene
    
    * added script and prefab folders + background and fox into the scene
    
    * added rigidbody and box collider to fox
    
    * added sprites for obstacles etc. + added them to the scene
    
    * made empty scripts to assets and added physics, tags, etc. to the rest of the sprites
    
    * current version of minigame finished, ready to make pull request
    
    * fixed problem with the falling logs not showing on screen

---
+++ Assets/minigameScripts/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    //mreference to player's speed
+    public float moveSpeed;
+    // reference to rigidbody of player
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // getting access to rigidbody component
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if touching screen, adding velocity to Player
+        // Input.GetMouseButton(0) should also work with touchscreen on mobile 
+        if (Input.GetMouseButton(0))
+        {
+            // world point means center is 0, left is negative and right is positive
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (touchPos.x < 0)
+            {
+                // if pressing on left side of the screen, player moves left
+                rigidBody.AddForce(Vector2.left * moveSpeed);
+            }
+            else
+            {
+                // if pressing on right side of the screen, player moves right
+                rigidBody.AddForce(Vector2.right * moveSpeed);
+            }
+        }
+        else
+        {
+            // if not touching screen, velocity is zero
+            rigidBody.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("Minigame");
+        }
+    }
+}
+
