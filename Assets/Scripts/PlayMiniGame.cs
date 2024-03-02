afcf2ddc62e294b3dd117674c6679b28839c3976
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2024-03-02 11:55:38 +0200

    cleaned folders, added play button

---
+++ Assets/Scripts/PlayMiniGame.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMiniGame : MonoBehaviour
+{
+    public string MiniGame = "Minigame";
+
+    private float touchArea = 0.1f;
+
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch kosketus = Input.GetTouch(0);
+
+            if (kosketus.phase == TouchPhase.Began && kosketus.deltaTime < touchArea)
+            {
+                Vector3 kosketusSijainti = Camera.main.ScreenToWorldPoint(kosketus.position);
+
+                Collider2D osuttuCollider = Physics2D.OverlapPoint(kosketusSijainti);
+
+                if (osuttuCollider != null)
+                {
+                    if (osuttuCollider.CompareTag("PlayMiniGame"))
+                    {
+                        SceneManager.LoadScene(MiniGame);
+                    }
+
+
+                }
+            }
+        }
+    }
+}
+
