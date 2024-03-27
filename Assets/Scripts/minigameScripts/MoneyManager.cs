165bc3fcd26e8edaccff06cf15a316852f6b215d
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2024-03-27 08:13:06 +0200

    did stuff

--- Assets/Scripts/minigameScripts/MoneyManager.cs
+++ Assets/Scripts/minigameScripts/MoneyManager.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+using TMPro;
 
 public class MoneyManager : MonoBehaviour
 {
     public static int money;
-    public Text moneyText;
+    //public Text moneyText;
+    public TextMeshProUGUI moneyText;
+
     public static MoneyManager instance;
+    // scoreText.text = "Score: " + score.ToString();
+
     // Start is called before the first frame update
     void Start()
     {
         money = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        moneyText.text = "Money: " + money;
+        moneyText.text = money.ToString();
+        Debug.Log("money amount" + money);
+        //moneyText.text = "Money: " + money;
+
     }
+
+
+
 
     private void Awake()
     {
