6e4fbf5744edc8bcbef78ab7e34da4fdc53dddbd
emiliavillanen <emilia.villanen@outlook.com>
2024-04-15 11:23:02 +0300

    noniiii bugi fiksattu

--- Assets/Scripts/backgroundMusicManager.cs
+++ Assets/Scripts/backgroundMusicManager.cs
@@ -8,19 +8,24 @@ public class backgroundMusicManager : MonoBehaviour
 
     public AudioSource bgMusic;
 
-    public static bool musicStart;
+    public static bool musicStart = true;
     public static bool plsPlay;
     public static bool plsStop;
     
 
     void Start()
     {
-        playBGMusic();
+        //playBGMusic();
     }
 
     
     void Update()
     {
+        if (musicStart == true)
+        {
+            playBGMusic();
+            musicStart = false;
+        }
         if (plsStop == true)
         {
             bgMusic.Pause();
@@ -38,5 +43,7 @@ public class backgroundMusicManager : MonoBehaviour
         bgMusic.Play();
         DontDestroyOnLoad(bgMusic);
     }
+
+    
 
 }
