47f57640991d9279fbbca32dd5762849a2bb4d79
Kakechu <98966226+Kakechu@users.noreply.github.com>
2024-04-16 08:41:30 +0300

    swipe to animal (#34)

---
+++ Assets/Scripts/AnimalNavigation/HedgehogNavigation.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HedgehogNavigation : MonoBehaviour
+{
+    public float swipeThreshold = 50f;
+
+    private Vector2 fingerDownPosition;
+    private Vector2 fingerUpPosition;
+
+    void Update()
+    {
+        CheckForSwipe();
+    }
+
+    void CheckForSwipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fingerDownPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            fingerUpPosition = Input.mousePosition;
+            CheckSwipe();
+        }
+    }
+
+    void CheckSwipe()
+    {
+        float swipeDistance = fingerUpPosition.x - fingerDownPosition.x;
+
+        if (Mathf.Abs(swipeDistance) > swipeThreshold)
+        {
+            if (swipeDistance < 0) // Swipe left (next)
+            {
+                SceneManager.LoadScene("squirrelView");
+            }
+
+            else if (swipeDistance > 0) // Swipe right (previous)
+            {
+                SceneManager.LoadScene("bunnyView");
+            }
+        }
+    }
+
+}
