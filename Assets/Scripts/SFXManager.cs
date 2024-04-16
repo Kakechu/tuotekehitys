9b93849695026ea0e4e2a17d09985323c236ea55
emiliavillanen <emilia.villanen@outlook.com>
2024-04-16 11:14:02 +0300

    added audio sources and references to them in script

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
+    AudioSource crunchSound;
+    AudioSource petSound;
+    AudioSource clickSound;
+
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        var SFXs = GetComponents(typeof(AudioSource)).Cast<AudioSource>().ToArray();
+        crunchSound = SFXs[0];
+        petSound = SFXs[1];
+        clickSound = SFXs[2];
     }
 
     // Update is called once per frame
